119ab11ffd455bc17126d3b73c79e3436e90bfa3
Iago Massucato <iagomassucatojl@gmail.com>
2023-05-03 22:06:04 -0300

    Calculos Simples

---
+++ src/main/java/com/mycompany/operadoresarit/OperadoresArit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operadoresarit;
+
+/**
+ *
+ * @author Microsoft
+ */
+public class OperadoresArit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
